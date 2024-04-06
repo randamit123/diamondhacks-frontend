c630a0e6ef79ddd1a2159f620c08d9989aeeedfe
abhip1106 <abhishekprabhu04@gmail.com>
2024-04-06 14:06:46 -0700

    FullStackConfiguration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+"use client";
 
+import React, {useEffect, useState} from "react";
+var inpData = 0
 export default function Home() {
+    useEffect(() => {
+      fetch("http://localhost:8080")
+      .then((response) => response.json())
+      .then((data) => {
+          inpData = data.message
+      })
+  })
+
   return (
-    <></>
+    inpData
   );
 }
