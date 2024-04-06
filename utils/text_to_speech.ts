40de6a8be8188059cebf46a91099bc07dd7b7fec
randamit123 <randamit123@gmail.com>
2024-04-06 13:01:20 -0700

    text to speech

---
+++ utils/text_to_speech.ts
@@ -0,0 +1,27 @@
+export async function getTextToSpeech(text: string) {
+  var apiKey = process.env.NEXT_PUBLIC_ELEVEN_API; // genereate new API key
+  var voiceId = ""; // select voice ID
+  const response = await fetch(
+    `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`,
+    {
+      method: "POST",
+      headers: {
+        accept: "audio/mpeg",
+        "xi-api-key": String(apiKey),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: text,
+        model_id: "eleven_monolingual_v1",
+        voice_settings: {
+          stability: 0.4,
+          similarity_boost: 0.8,
+        },
+      }),
+    }
+  );
+
+  const blob = await response.blob();
+  const audioUrl = URL.createObjectURL(blob);
+  return audioUrl;
+}
