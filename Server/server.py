8a37a268631a47090de266f4199a023f6aad4137
Aman Desai <amanpdesai@gmail.com>
2024-04-07 04:25:08 -0700

    getting there with webcam display

--- Server/server.py
+++ Server/server.py
@@ -1,97 +1,39 @@
+import eventlet
+eventlet.monkey_patch()
+
 import cv2
-import mediapipe as mp
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, render_template
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-#CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+camera=cv2.VideoCapture(0)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+#CORS(app)
+socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="http://localhost:3000")
 
-
-@app.route('/')
-def hello_world():
-    return jsonify({'message': "Hello World"}
-                   )
-""" def generate_frames():
-    cap = cv2.VideoCapture(0)
-    with mp.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            # Process frame with MediaPipe Holistic
-            # Perform your landmark detection and pose estimation here
-            # Example:
-            # results = holistic.process(frame)
-
-            # Convert processed frame to JPEG format
-            # Example: Encode processed frame to JPEG bytes
-            # encoded_frame = cv2.imencode('.jpg', frame)[1].tobytes()
-
-            # Yield processed frame as byte stream
-            # Example: yield encoded_frame
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
-
-    cap.release() """
-
-""" def generate_frames():
-    print("test")
-    cap = cv2.VideoCapture(0)
-    with mp.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            # Process frame with MediaPipe Holistic
-            # Example:
-            # results = holistic.process(frame)
-            
-            # Encode frame as JPEG bytes
-            _, buffer = cv2.imencode('.jpg', frame)
-            frame_bytes = buffer.tobytes()
-
-            # Emit frame data over WebSocket
-            socketio.emit('frame', {'image': frame_bytes}, namespace='/')
-
-    cap.release()
-
-@socketio.on('connect', namespace='/home')
-def ws_connect():
-    print('Client connected')
-    emit('server_response', {'data': 'Connected'})
-
-@socketio.on('disconnect', namespace='/home')
-def ws_disconnect():
-    print('Client disconnected')
-
-@app.route('/home')
-def index():
-    print("Streaming frames...")
-    socketio.start_background_task(target=generate_frames)
-    return "Streaming frames..."
- """
 
 def generate_frames():
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
             break
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame_bytes = buffer.tobytes()
-        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
 
-@app.route('/video_feed')
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
-""" if __name__ == '__main__':
-    #app.run(debug=True, port=8080)
-    socketio.run(app, debug=True, port=8080) """
+    eventlet.wsgi.server(eventlet.listen(('', 8080)), app)
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,54 +2,13 @@
 "use client"
 import React, { useEffect, useRef } from "react";
 import { signOut } from "next-auth/react";
-import { io, Socket } from "socket.io-client";
-import WebSocketCall from "../components/WebSocketCall"
+import WebcamStream from "../components/webcamComponent"
 
 export default function Home() {
-  const videoRef = useRef<HTMLImageElement>(null);
-  const socketRef = useRef<Socket | null>(null);
-
-  useEffect(() => {
-    const socket = io("ws://localhost:8080/home"); // Connect to your Flask server WebSocket endpoint
-
-    socketRef.current = socket;
-
-    socket.on("connect", () => {
-      console.log("WebSocket connected");
-    });
-
-    socket.on("frame", (data: ArrayBuffer) => {
-      // Convert ArrayBuffer to Blob
-      const blob = new Blob([data], { type: "image/jpeg" });
-      const imageUrl = URL.createObjectURL(blob);
-
-      // Update the src attribute of the videoRef element with the new image
-      if (videoRef.current) {
-        videoRef.current.src = imageUrl;
-      }
-    });
-
-    socket.on("disconnect", () => {
-      console.log("WebSocket disconnected");
-    });
-
-    return () => {
-      socket.disconnect(); // Cleanup WebSocket connection on component unmount
-    };
-  }, []);
 
   return (
     <>
-      {videoRef.current && (
-        <img
-          ref={videoRef}
-          alt="Webcam Feed"
-          style={{ width: "100%" }}
-        />
-      )}
-      {/* Render the WebSocketCall component and pass the socket instance */}
-      {socketRef.current && <WebSocketCall socket={socketRef.current}></WebSocketCall>}
-      {console.log(socketRef.current)}
+      <WebcamStream></WebcamStream>
       <button
         className="bg-white rounded-full border border-gray-200 text-gray-800 px-4 py-2 flex items-center space-x-2 hover:bg-gray-200"
         onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
